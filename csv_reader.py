3d1ed088c76592540197949289d0e30e7a9aed64
danijeljw-rpc <danijel@repasscloud.com>
2023-04-21 18:47:46 +1000

    csv reader now has 2 options

--- csv_reader.py
+++ csv_reader.py
@@ -8,5 +8,5 @@ def read_csv_file(csv_file_path):
             current_date = datetime.datetime.now().strftime('%Y%m%d')
             print(f"Current gh-actions server date: {current_date}")
             if row[0] == current_date:
-                return row[1]
+                return row[1], row[2]
     return None
